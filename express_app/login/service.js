292acaf54317a4a1d870c01578047fba601ebae1
Gireesh Babu <gireesh.babu@corelogic.co.uk>
2014-12-31 01:18:54 +0530

    Added a login service

---
+++ express_app/login/service.js
@@ -0,0 +1,35 @@
+
+module.exports = 
+	function(){
+	
+		var pg = require('pg');
+		var _ = require('underscore');
+	
+		this.authenticate = function(request, response){
+			//console.log("request =" + request.body);
+			var loginName = request.param('loginName');
+			var password = request.param('password');
+			//console.log("request received loginName - "+ loginName +"| password - "+ password );
+			
+			pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+			    client.query('SELECT * FROM user_login_info WHERE login_name=$1 AND password=$2',[loginName,password], function(err, result) {
+			      done();
+			      if (err)
+			       { console.error(err); response.send("Error " + err); }
+			      else
+			       { 
+			    	  if(_.size(result.rows) > 0)
+			    	  {
+			    		  response.json("{Authentication: SUCCESS}");
+			    	  } 
+			    	  else
+			    	  {
+			    		  response.status(401);
+			    	  	  response.json("{Authentication :FAILED}");
+			    	  }
+			       }
+			    });
+			  });
+			}
+	};
+
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var express = require('express');
-var _ = require('underscore');
+
 var bodyParser = require('body-parser')
 var app = express();
-var pg = require('pg');
 
+var LoginService = require('./express_app/login/service.js');
+var loginService = new LoginService();
+
+console.log(LoginService);
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json())
@@ -14,28 +17,7 @@ app.get('/', function(request, response) {
 
 app.post('/login', function(request, response) {
 	
-	//console.log("request =" + request.body);
-	var loginName = request.param('loginName');
-	var password = request.param('password');
-	//console.log("request received loginName - "+ loginName +"| password - "+ password );
-	
-	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-	    client.query('SELECT * FROM user_login_info WHERE login_name=$1 AND password=$2',[loginName,password], function(err, result) {
-	      done();
-	      if (err)
-	       { console.error(err); response.send("Error " + err); }
-	      else
-	       { 
-	    	  if(_.size(result.rows) > 0)
-	    		  response.json("{Authentication: SUCCESS}");
-	    	  else
-	    		  {
-	    		  response.status(401);
-	    	  	  response.json("{Authentication :FAILED}");
-	    		  }
-	       }
-	    });
-	  });
+	loginService.authenticate(request,response);
 	  
 });
 
