ce8d335dcd778976216367315afbf3fc8dcc21f2
Gireesh Babu <gireesh.babu@corelogic.co.uk>
2015-01-16 04:35:29 +0530

    added ddl scripts

---
+++ config/pg-schema-ddl.sql
@@ -0,0 +1,5 @@
+create table advertisement_data(add_id integer not null, user_fk integer not null, file bytea, file_type varchar(20) );
+
+create table device_register_map(key varchar(100) unique not null, mobile_num varchar(20));
+
+create table user_login_info(login_name varchar(25) unique not null, password varchar(100));
